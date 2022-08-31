95d750b44bc41296964fc0130d18ca8686078a0b
Charles Hildebrant <ceh2624@gmail.com>
2022-08-31 00:06:16 -0400

    Add project files.

---
+++ Fib/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 0, b = 1, c = 0;
+int numOfFibs;
+Console.Write("How many fib numbers do you want? ");
+#pragma warning disable CS8604 // get rid of "Possible null reference argument"
+numOfFibs = int.Parse(Console.ReadLine());
+#pragma warning restore CS8604 
+Console.Write("{0}, {1},", a, b);
+for (int x = 2; x < numOfFibs; x++)
+{
+    c = a + b;
+    Console.Write(" {0},", c);
+    a = b;
+    b = c;
+}
